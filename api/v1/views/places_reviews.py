bcddaf543d6a527fadaac9dca171fe74f06ba40a
Chimmysmallz <105133066+Chimmysmallz@users.noreply.github.com>
2023-01-23 13:54:26 +0100

    Update places_reviews.py

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,75 +1,56 @@
-#!/usr/bin/python3
-"""
-Module to interface with the link between Places and Amenities
-"""
-from api.v1.views import (app_views, Place, Review, storage)
-from flask import (request, jsonify, abort)
+from flask import request
+from flask_restful import Resource
+from api.v1.models.places import Place
+from api.v1.models.cities import City
+from api.v1.models.users import User
 
-#from api.v1.views import get_linked
+class PlaceView(Resource):
+    def get(self, city_id, place_id=None):
+        if place_id:
+            place = Place.get(place_id)
+            if place:
+                return place.to_dict(), 200
+            else:
+                return {}, 404
+        else:
+            city = City.get(city_id)
+            if city:
+                places = city.places
+                return [place.to_dict() for place in places], 200
+            else:
+                return {}, 404
 
+    def post(self, city_id):
+        data = request.get_json()
+        if not data:
+            return {'message': 'Not a JSON'}, 400
+        if 'user_id' not in data:
+            return {'message': 'Missing user_id'}, 400
+        user = User.get(data['user_id'])
+        if not user:
+            return {}, 404
+        if 'name' not in data:
+            return {'message': 'Missing name'}, 400
+        place = Place(name=data['name'], user_id=data['user_id'], city_id=city_id)
+        place.save()
+        return place.to_dict(), 201
 
-@app_views.route('/places/<place_id>/reviews',
-                 methods=['GET', 'POST'],
-                 strict_slashes=False)
-def review_by_place(place_id=None):
-    """
-    Access the api call with on a place object to get its reviews
-    returns a 404 if not found.
-    - POST: Creates a new review object with the place_object linked
-    - GET: Default, returns all review objects linked to the place.
-    """
-    if place_id not in storage.all('Place'):
-        abort(404)
+    def put(self, place_id):
+        data = request.get_json()
+        if not data:
+            return {'message': 'Not a JSON'}, 400
+        place = Place.get(place_id)
+        if not place:
+            return {}, 404
+        for key, value in data.items():
+            if key not in ['id', 'user_id', 'city_id', 'created_at', 'updated_at']:
+                setattr(place, key, value)
+        place.save()
+        return place.to_dict(), 200
 
-    if request.method == 'POST':
-        posted_obj = request.get_json()
-        if posted_obj is None:
-            return("Not a JSON", 400)
-        if 'name' not in posted_obj:
-            return("Missing name", 400)
-        new_obj = State(**posted_obj)
-        storage.save()
-        return(jsonify(new_obj.to_json()), 201)
-
-#    all_reviews = storage.get('Place', place_id).review
-#    rtn_json = []
-#    for review in all_reviews:
-#        rtn_json.append(review.to_json())
-    return(get_linked('Place', place_id, review))
-
-
-
-@app_views.route('reviews/<review_id>',
-                 methods=['GET', 'DELETE', 'PUT'],
-                 strict_slashes=False)
-def all_reviews(review_id=None):
-    """
-    Access the api call with on a place object to get its reviews
-    returns a 404 if not found.
-    - GET: Default, gets a review at <review_id>, status 200
-    - DELETE: Deletes the review at id. Returns '{}', status 200
-    - PUT:
-    """
-    if review_id not in storage.all('Review'):
-        abort(404)
-
-    if request.method == 'DELETE':
-        storage.delete(storage.get('Review', review_id))
-        storage.save()
-        return(jsonify({}))
-
-    if request.method == 'PUT':
-        put_obj = request.get_json()
-        if put_obj is None:
-            return("Not a JSON", 400)
-        instance = storage.get('Review', review_id)
-        ignore_keys = ['id', 'user_id', 'place_id', 'updated_at']
-        for attrib in put_obj:
-            if attrib not in ignore_keys:
-                setattr(instance, attrib, put_obj[attrib])
-        instance.save()
-        return(jsonify(instance.to_json()))
-
-    """ Default: GET """
-    instance_get = storage.get('Review', review_id)
-    return(jsonify(instance_get.to_json()))
+    def delete(self, place_id):
+        place = Place.get(place_id)
+        if not place:
+            return {}, 404
+        place.delete()
+        return {}, 200
